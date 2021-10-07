138dccec5d1c1c27ee2163e38a3a4e3e98d77bf5
PauloAragao <paulotorres12@gmail.com>
2021-10-07 20:12:24 -0300

    Player movement adjust + new animations imported

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    
+    float translation;
+    float strafe;
+ 
+     void FixedUpdate () {
+        if(Input.GetAxis("Vertical") != 0f || Input.GetAxis("Horizontal") != 0f )
+        {
+            transform.Translate(Vector3.forward * (moveSpeed) * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,15 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private Animator _animator;
+    [SerializeField] private Transform _camera;
 
 
+    private bool _rolling = false;
+    private float _moveSpeedOriginal;
+    private float _moveSpeedRolling;
+
+    private Vector3 _lastLookDiretion;
+    private Vector3 _moveDirection;
     #endregion
 
     #region Constants
@@ -21,39 +28,88 @@ public class PlayerMovement : MonoBehaviour
     private float _GRAVITYFORCE = 9.8f;
 
     #endregion
-    private Vector3 _lastLookDiretion;
-    private Vector3 _moveDirection;
+    
 
     void Start()
     {
+        _moveSpeedOriginal = _moveSpeed;
+        _moveSpeedRolling = _moveSpeed *1.7f;
+        _rolling = false;
         _moveDirection = Vector3.zero;
         _lastLookDiretion = Vector3.up;
     }
 
     private void FixedUpdate() 
     {
-        if (_characterController.isGrounded)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput,0,verticalInput).normalized;
+        if(direction.magnitude >= 0.1f)
         {
-            _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            _moveDirection *= _moveSpeed;
+            float directionAngle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
+        }
+        /*
+        if (_characterController.isGrounded && !_rolling)
+        {
+            if(Input.GetAxis("Vertical") != 0f )
+            {
+                _moveDirection = (transform.position - _camera.position) * Input.GetAxis("Vertical");
+                
+            }
+            if(Input.GetAxis("Horizontal") != 0f)
+            {
+                _moveDirection = _moveDirection + new Vector3(Input.GetAxis("Horizontal"),0,0);
+            }
+            if(Input.GetAxis("Vertical") == 0f && Input.GetAxis("Horizontal") == 0f)
+            {
+                _moveDirection = Vector3.zero;
+            }
+            //_moveDirection *= _moveSpeed;
             if (Input.GetButton("Jump"))
             {
                 _animator.SetTrigger("Jump");
                 _moveDirection.y = _jumpForce;
             }
-        }
-        if(_moveDirection == Vector3.zero)
+            else if (Input.GetButton("Fire1"))
+            {
+                _animator.SetTrigger("Roll");
+            }
+        }*/
+        if(new Vector3(direction.x,0,direction.z) == Vector3.zero)
         {
-            if(_lastLookDiretion != Vector3.zero)
+            if(new Vector3(_lastLookDiretion.x,0,_lastLookDiretion.z) != Vector3.zero)
             transform.rotation = Quaternion.LookRotation(new Vector3(_lastLookDiretion.x,0,_lastLookDiretion.z));
         }
         else
         {
-            transform.rotation = Quaternion.LookRotation(new Vector3(_moveDirection.x,0,_moveDirection.z));
-            _lastLookDiretion = _moveDirection;
+            transform.rotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+            _lastLookDiretion = direction;
         }
-        _moveDirection.y -= _GRAVITYFORCE*_gravityFactor * Time.deltaTime;
-        _animator.SetFloat("MoveSpeed",Mathf.Abs(_moveDirection.x) + Mathf.Abs(_moveDirection.z));
-        _characterController.Move(_moveDirection * Time.deltaTime);
+        direction.y -= _GRAVITYFORCE*_gravityFactor * Time.deltaTime;
+        _animator.SetFloat("MoveSpeed",Mathf.Abs(direction.x) + Mathf.Abs(direction.z));
+        _characterController.Move(direction *_moveSpeed* Time.deltaTime);
     }
+    #region Methods
+    public void StartRolling()
+    {
+        _moveSpeed = _moveSpeedRolling;
+        _rolling = true;
+        Debug.Log("start");
+
+    }
+    public void MidRolling()
+    {
+        _moveSpeed = 0f;
+        Debug.Log("mid");
+
+    }
+    public void EndRolling()
+    {
+        _moveSpeed = _moveSpeedOriginal;
+        _rolling = false;
+        _animator.ResetTrigger("Roll");
+        Debug.Log("End");
+    }
+         
+    #endregion
 }
