97923848ec045282c4f3aacc6e1792ef52744e69
PauloAragao <paulotorres12@gmail.com>
2021-10-07 20:42:31 -0300

    Camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _jumpForce = 7.0f;
     [SerializeField] private float _moveSpeed = 10f;
     [SerializeField] private float _gravityFactor = 1f;
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+
+    [SerializeField] private float _turnSmoothVelocity = 0.1f;
     [Header("References")]
     [SerializeField] private CharacterController _characterController;
     [SerializeField] private Animator _animator;
@@ -43,51 +46,36 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        float directionAngle = 0f; 
         Vector3 direction = new Vector3(horizontalInput,0,verticalInput).normalized;
-        if(direction.magnitude >= 0.1f)
-        {
-            float directionAngle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
-        }
-        /*
         if (_characterController.isGrounded && !_rolling)
         {
-            if(Input.GetAxis("Vertical") != 0f )
+            if(direction.magnitude >= 0.1f)
             {
-                _moveDirection = (transform.position - _camera.position) * Input.GetAxis("Vertical");
+                directionAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,directionAngle,ref _turnSmoothVelocity,_turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f,angle,0f);
+                _moveDirection = Quaternion.Euler(0f,directionAngle,0f) * Vector3.forward;
                 
             }
-            if(Input.GetAxis("Horizontal") != 0f)
-            {
-                _moveDirection = _moveDirection + new Vector3(Input.GetAxis("Horizontal"),0,0);
-            }
-            if(Input.GetAxis("Vertical") == 0f && Input.GetAxis("Horizontal") == 0f)
+            else
             {
                 _moveDirection = Vector3.zero;
             }
-            //_moveDirection *= _moveSpeed;
             if (Input.GetButton("Jump"))
             {
                 _animator.SetTrigger("Jump");
-                _moveDirection.y = _jumpForce;
+                direction.y = _jumpForce;
             }
             else if (Input.GetButton("Fire1"))
             {
                 _animator.SetTrigger("Roll");
             }
-        }*/
-        if(new Vector3(direction.x,0,direction.z) == Vector3.zero)
-        {
-            if(new Vector3(_lastLookDiretion.x,0,_lastLookDiretion.z) != Vector3.zero)
-            transform.rotation = Quaternion.LookRotation(new Vector3(_lastLookDiretion.x,0,_lastLookDiretion.z));
         }
-        else
-        {
-            transform.rotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
-            _lastLookDiretion = direction;
-        }
-        direction.y -= _GRAVITYFORCE*_gravityFactor * Time.deltaTime;
         _animator.SetFloat("MoveSpeed",Mathf.Abs(direction.x) + Mathf.Abs(direction.z));
-        _characterController.Move(direction *_moveSpeed* Time.deltaTime);
+        _moveDirection.y -= _GRAVITYFORCE*_gravityFactor * Time.deltaTime;
+        _characterController.Move(_moveDirection *_moveSpeed* Time.deltaTime);
+        
     }
     #region Methods
     public void StartRolling()
