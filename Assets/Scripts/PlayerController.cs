42ab69693428ef6e42221c2708fedfac2e07ad97
PauloAragao <paulotorres12@gmail.com>
2021-10-09 01:39:32 -0300

    Traps created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Cinemachine;
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float rotationSpeed;
-    
-    float translation;
-    float strafe;
- 
-     void FixedUpdate () {
-        if(Input.GetAxis("Vertical") != 0f || Input.GetAxis("Horizontal") != 0f )
+    #region Variables
+    [SerializeField] private int _hp = 10;
+    [SerializeField] private int _coins = 0;
+    [Header("References")]
+    [SerializeField] private GameObject _ragdoll;
+    [SerializeField] private CinemachineFreeLook _cameraVitual;
+
+
+    #endregion
+
+    #region Methods
+    public void TakeDamage(int damage)
+    {
+        _hp -= damage;
+        if(_hp < 0)
         {
-            transform.Translate(Vector3.forward * (moveSpeed) * Time.deltaTime);
+            Die();
         }
     }
+    private void Die()
+    {
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<CharacterController>().enabled = false;
+        GetComponent<Animator>().enabled = false;
+        _cameraVitual.LookAt = _ragdoll.transform;
+        _cameraVitual.Follow = _ragdoll.transform;
+    }
+    #endregion
 }
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private int _damage = 10;         
+    #endregion
+    #region MonoBehaviour
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("acertou");
+            other.gameObject.GetComponent<PlayerController>().TakeDamage(_damage);
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("acertou");
+            other.gameObject.GetComponent<PlayerController>().TakeDamage(_damage);
+        }
+    }
+         
+    #endregion
+
+}
